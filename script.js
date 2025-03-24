e5b1935739abb008074937c3ce666e3b95a30bb7
OLIVEMR2008 <149404368+OLIVEMR2008@users.noreply.github.com>
2025-03-24 10:39:42 -0300

    .

--- script.js
+++ script.js
@@ -30,3 +30,7 @@ const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') ===
 const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
 botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
